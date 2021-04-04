8df29e5cd510c9579018930777e65b451907556c
Михаил <migel.bd@gmail.com>
2021-04-04 19:09:46 +0300

    init

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-site-settings",
+    version="0.1",
+    scripts=[],
+    author="Mikhail Badrazhan",
+    author_email="migel.bd@gmail.com",
+    description="Site settings management",
+    long_description=long_description,
+    install_requires=[
+    ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/migelbd/django-site-settings",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
